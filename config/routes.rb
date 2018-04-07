f1b1cc3d83fcf494e22bfcb6d1f712001b161cb0
Eduardo Leme <eddyleme@gmail.com>
2018-04-07 12:50:20 -0400

    generate password reset controller, add columns to admins for password reset

--- config/routes.rb
+++ config/routes.rb
@@ -1,52 +1,10 @@
 Rails.application.routes.draw do
-  get 'homes/index'
-
-  get 'homes/contact'
-
-  get 'tickets/index'
-
-  get 'tickets/show'
-
-  get 'tickets/new'
-
-  get 'tickets/update'
-
-  get 'photos/index'
-
-  get 'photos/show'
-
-  get 'photos/new'
-
-  get 'photos/update'
-
-  get 'packages/index'
-
-  get 'packages/show'
-
-  get 'packages/new'
-
-  get 'packages/update'
-
-  get 'albums/index'
-
-  get 'albums/show'
-
-  get 'albums/new'
-
-  get 'albums/update'
-
-  get 'events/index'
-
-  get 'events/show'
-
-  get 'events/new'
-
-  get 'events/update'
-
   get '/login' => 'sessions#new'
   root 'home#index'
   resource :sessions
 
 	delete '/logout' => 'sessions#destroy'
 	get '/logout' => 'sessions#destroy'
+
+	resources :password_resets,     only: [:new, :create, :edit, :update]
 end
