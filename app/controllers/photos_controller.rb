7df15a4cb2f034528327ac3ca0f48b9ae6e0b7ef
Jaye Brisard <jayebrisard20@gmail.com>
2018-04-07 14:59:42 -0400

    controller variable update

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,11 +1,14 @@
 class PhotosController < ApplicationController
   def index
+    @photos = Photo.all
   end
 
   def show
+    @photo = Photo.find(params[:id])
   end
 
   def new
+    @photos = Photo.new
   end
 
   def update
--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -1,11 +1,14 @@
 class TicketsController < ApplicationController
   def index
+    @tickets = Ticket.all
   end
 
   def show
+    @ticket = Ticket.find(params[:id])
   end
 
   def new
+    @tickets = Ticket.new
   end
 
   def update
