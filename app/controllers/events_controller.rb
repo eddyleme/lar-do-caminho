e5d436926e19b33ddbb5b4941937bb89a607e152
Eduardo Leme <eddyleme@gmail.com>
2018-04-10 13:19:28 -0400

    add event form and delete link to event index

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,25 +1,51 @@
 class EventsController < ApplicationController
-  before_action :set_event, only: [:show]
+  before_action :set_event, only: [:show, :destroy]
   before_action :logged_in?
 
   def index
     @events = Event.all
+    @event = Event.new
   end
 
   def show
     @package = Package.new
     @ticket = Ticket.new
-    @tickets = Ticket.all
   end
 
   def new
     @event = Event.new
   end
 
+  def create
+    @event = Event.new(event_params)
+
+    respond_to do |format|
+      if @event.save
+        format.html { redirect_to events_path, notice: 'Event was successfully added.' }
+        format.json { render :index, status: :created, location: @event }
+      else
+        format.html { render :new }
+        format.json { render json: @event.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
   def update
   end
 
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_path, notice: 'Event was successfully deleted.' }
+      format.json { head :no_content }
+    end
+  end
+
   private
+
+  def event_params
+    params.require(:event).permit(:name, :description, :date)
+  end
 
   def set_event
     @event = Event.find(params[:id])
